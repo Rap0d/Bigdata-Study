f290f0d4bdfef7c30ed9fe5700e816e33da507e0
rap0d <changsh9155@naver.com>
2019-08-16 11:45:41 +0900

    Delete test file

--- JvProject/src/Jv_190816_01/Test_1.java
+++ JvProject/src/Jv_190816_01/Test_1.java
@@ -1,10 +1,10 @@
-package Test1;
+package Jv_190816_01;
 
 public class Test_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		System.out.println("test Korean : 한글");
 	}
 
 }
