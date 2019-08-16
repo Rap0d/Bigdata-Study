d95c8819ba3769b85572f1e00637e67fb7af1207
rap0d <changsh9155@naver.com>
2019-08-16 16:46:25 +0900

    update files

--- JvProject/src/Jv_190816_01/PrintEx.java
+++ JvProject/src/Jv_190816_01/PrintEx.java
@@ -4,23 +4,33 @@ public class PrintEx {
     public static void main(String[] args) {
         int i = 65;
         char ch = 'a';
-        double d = 12.345678;
+        double d = 12.3456789;
         int age = 35;
         String name = "홍길동";
 
         System.out.println(d);
         System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        System.out.printf("실수1 : [%f]\n", d);
-        
+        System.out.printf("실수2 : [%10.2f]\n", d);
+        System.out.printf("실수3 : [%-10.2f]\n", d);
+
+        System.out.printf("이름은 [%s], 나이는 [%d]\n", name, age);
+
+        System.out.printf("정수를 문자로 : [%c]\n문자를 정수로 : [%d]\n", (char) i, (int) ch);
+
+        while (true) {
+
+            if (i >= 91 && i <= 96) {
+                i++;
+                continue;
+            }
+
+            System.out.println(i + " : " + (char) i);
+            i++;
+
+            if ((char) i == 'z') {
+                System.out.println(i + " : " + (char) i);
+                break;
+            }
+        }
     }
 }
