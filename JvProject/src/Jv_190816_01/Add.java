a7bae0c06192f6c31d7f1cd38a619ef45644e581
rap0d <changsh9155@naver.com>
2019-08-16 14:25:33 +0900

    create file

---
+++ JvProject/src/Jv_190816_01/Add.java
@@ -0,0 +1,34 @@
+package Jv_190816_01;
+
+import java.util.Scanner;
+
+public class Add {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int y, z;
+        // System.out.print("num : ");
+        // x = sc.nextInt();
+        x = 3;
+        y = 5;
+
+        z = x + y;
+
+        System.out.println(x + " 더하기 " + y + "는 " + z + "입니다.");
+
+        x = 5;
+        y = 4;
+        z = x * y;
+
+        System.out.println(x + " 곱하기 " + y + "는 " + z + "입니다.");
+
+        x = 3;
+        y = 5;
+        z = 2 * x + 3 * y - 2;
+
+        System.out.println("x : " + x);
+        System.out.println("y : " + y);
+        System.out.println("z : " + z);
+        sc.close();
+    }
+}
---
+++ JvProject/src/Jv_190816_01/Second.java
@@ -0,0 +1,7 @@
+package Jv_190816_01;
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
