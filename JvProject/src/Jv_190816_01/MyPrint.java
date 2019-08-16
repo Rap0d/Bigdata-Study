d05503903ff6c5dfa292322164d8ee2ec996dbd0
rap0d <changsh9155@naver.com>
2019-08-16 12:42:11 +0900

    Update file

---
+++ JvProject/src/Jv_190816_01/MyPrint.java
@@ -0,0 +1,34 @@
+package Jv_190816_01;
+/* 
+
+Multi Line Comment
+
+Date : 2019-08-16
+
+Name : Chang sh
+
+*/
+public class MyPrint {
+	public static void main(String args[]) {
+		// System.out.println("홍");
+		// System.out.println();
+		// System.out.println("길");
+		// System.out.println("동");
+		// System.out.println("");
+		System.out.println("홍길동");
+		
+		System.out.print("안녕\t");
+		System.out.print("김연아\n");
+		
+		System.out.print("*");System.out.print("*");System.out.print("*");
+		System.out.println();
+		System.out.print("*");System.out.print("*\n\n");
+		
+		System.out.println("\"예체능\"프로그램\n\n메인 MC\'강호동\'\n");
+		
+		System.out.println("이름\t국어\t영어\t수학");
+		System.out.println("홍길동\t70\t60\t50\t");
+		System.out.println("박영희\t50\t80\t40\t\n");
+		// 주석처리
+	}
+}
--- JvProject/src/Jv_190816_01/Test_1.java
+++ JvProject/src/Jv_190816_01/Test_1.java
@@ -3,7 +3,6 @@ package Jv_190816_01;
 public class Test_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("test Korean : 한글");
 	}
 
