d9364e85f754db1b93d1df215e3f9167785e8d96
rap0d <changsh9155@naver.com>
2019-08-20 09:57:23 +0900

    update file

--- JvProject/src/Jv_190819_02/MyPay.java
+++ JvProject/src/Jv_190819_02/MyPay.java
@@ -8,16 +8,18 @@ import java.util.Random;
 public class MyPay {
 
     public static void main(String[] args) {
-        final int RATE = 5001;
+        final int RATE = 5000;
         double pay = 0;
         int hours = 0;
 
         hours = new Random().nextInt(19)+1;
 
-        if (hours <= 8) {
+        if (hours <= 10) {
             pay += hours * RATE;
-        } else {
-            pay += (((hours - 8) * RATE) * 1.5) + (8 * RATE);
+        } else if(hours > 10 && hours <= 20) {
+            pay += (((hours - 10) * RATE) * 1.5) + (10 * RATE);
+        } else if(hours > 20) {
+            pay += (((hours - 20) * RATE) * 2) + (20 * RATE);
         }
 
         System.out.println(hours + "시간 근무시, 임금은 " + pay + "입니다.");
