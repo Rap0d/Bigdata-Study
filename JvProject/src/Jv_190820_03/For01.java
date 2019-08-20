1644c52bd4b37ef271a467d5871c55baca84bb4f
rap0d <changsh9155@naver.com>
2019-08-20 17:31:18 +0900

    update file

--- JvProject/src/Jv_190820_03/For01.java
+++ JvProject/src/Jv_190820_03/For01.java
@@ -17,18 +17,102 @@ public class For01 {
 
         Star(0, 10);
         Star2(4, 10);
-        
-        For02(20);
+
+        For02(10);
+        For04(10);
+        For05(10);
+
+        Differ(50);
+
+        SumTest(10);
+        TotalSum(100);
+
+        For06(100);
+    }
+
+    private static void For06(int max) {
+        double sum = 0.0;
+
+        for (int i = 1; i <= max; i++) {
+            sum += i%2==0 ? (-1 / (double)i) : (1 / (double)i);
+        }
+        System.out.println("sum : " + sum);
+    }
+
+    private static void TotalSum(int max) {
+        double sum = 0.0;
+        for (int i = 1; i <= max; i++) {
+            sum += (double)(i-1)/i;
+        }
+        System.out.println("sum : " + sum);
+    }
+
+    private static void SumTest(int max) {
+        int sumA = 0; int sumB = 0; int sumC = 0;
+
+        for (int i = 1; i <= max; i++) {
+            if(i%3==0)
+            sumA += i;
+            else if (i >= 5 && i != 8)
+            sumB += i;
+            else if (i < 5)
+            sumC += i;
+        }
+
+        System.out.println("sumA : " + sumA + ", sumB : " + sumB + ", sumC : " + sumC);
+    }
+
+    private static void Differ(int max) {
+        int sumA = 0;
+        int sumB = 0;
+
+        for (int i = 1; i <= max; i++) {
+            if(i%3==0)
+            sumA += i;
+            else
+            sumB += i;
+        }
+        System.out.println("sumA : " + sumA + " - sumB : " + sumB + " = " + (sumA - sumB));
+    }
+
+    private static void For05(int max) {
+        int sum = 0;
+        for (int i = 1; i <= max; i++) {
+            if(i%3 == 0 || i%5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("sum : " + sum);
+    }
+
+    private static void For04(int max) {
+        int sum0 = 0, sum1 = 0, sum2 = 0;
+        for (int i = 1; i <= max; i++) {
+            switch (i % 3) {
+            case 0:
+                sum0 += i;
+                break;
+            case 1:
+                sum1 += i;
+                break;
+            case 2:
+                sum2 += i;
+                break;
+            }
+        }
+        System.out.println("sum 0 : " + sum0);
+        System.out.println("sum 1 : " + sum1);
+        System.out.println("sum 2 : " + sum2);
     }
 
     private static void For02(int max) {
         int odd = 0, even = 0;
 
         for (int i = 1; i <= max; i++) {
-            if(i%2 == 0)
-            even += i;
+            if (i % 2 == 0)
+                even += i;
             else
-            odd += i;
+                odd += i;
         }
 
         System.out.println("Odd : " + odd);
@@ -38,13 +122,13 @@ public class For01 {
     private static void Star2(int st, int max) {
         for (int i = 1; i <= max; i++) {
             System.out.print("*");
-            if(i%st == 0)
-            System.out.println();
+            if (i % st == 0)
+                System.out.println();
         }
     }
 
     private static void Star(int row1, int col1) {
-        for(int i = 1; i <= col1; i++) {
+        for (int i = 1; i <= col1; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
@@ -53,7 +137,7 @@ public class For01 {
     }
 
     private static void Pow(int num) {
-        for (int i = 1; i < num+1; i++) {
+        for (int i = 1; i < num + 1; i++) {
             System.out.println(i + "의 제곱은 " + (i * i));
         }
     }
@@ -61,7 +145,7 @@ public class For01 {
     private static void OneToTen(int su, int j) {
         int total = 0;
 
-        for (int i = j; i < su+1; i++) {
+        for (int i = j; i < su + 1; i++) {
             total += i;
         }
         System.out.println("total : " + total);
@@ -69,7 +153,7 @@ public class For01 {
 
     private static void Ex01(int st, int max, int ad) {
         int total = 0;
-        for (int i = st; i < max; i+=ad) {
+        for (int i = st; i < max; i += ad) {
             total += i;
         }
         System.out.println("Res : " + total);
@@ -77,7 +161,7 @@ public class For01 {
 
     private static void Ex02(int st, int min, int ad) {
         int total = 0;
-        for (int i = st; i > min; i+=ad) {
+        for (int i = st; i > min; i += ad) {
             total += i;
         }
         System.out.println("Res : " + total);
@@ -85,7 +169,7 @@ public class For01 {
 
     private static void Ex03(int st, int max, int ad) {
         int total = 0;
-        for (int i = st; i < max; i+=ad) {
+        for (int i = st; i < max; i += ad) {
             total += i * i;
         }
         System.out.println("Res : " + total);
