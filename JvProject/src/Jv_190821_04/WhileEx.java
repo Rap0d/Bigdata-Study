c21a2197d8953c75cf57553d541d9f3b86d78092
rap0d <changsh9155@naver.com>
2019-08-21 10:43:10 +0900

    delete fx folder

--- JvProject/src/Jv_190821_04/WhileEx.java
+++ JvProject/src/Jv_190821_04/WhileEx.java
@@ -2,14 +2,42 @@ package Jv_190821_04;
 
 public class WhileEx {
 	public static void main(String[] args) {
-		Ex1();
+		// Ex1();
+		Ex2(100);
+	}
+
+	private static void Ex2(int max) {
+		int total1 = 0;
+		int total2 = 0;
+		int total3 = 0;
+
+		int i = 1;
+		while (i <= max) {
+			total1 += i;
+			i += 3;
+		}
+		System.out.println("total1 : " + total1);
+		
+		i = 3;
+		while (i <= max) {
+			total2 += (max-i);
+			i += 5;
+		}
+		System.out.println("total2 : " + total2);
+
+		i = 1;
+		while (i <= max) {
+			total3 += (i*i);
+			i += 5;
+		}
+		System.out.println("total3 : " + total3);
 	}
 
 	private static void Ex1() {
 		int total = 0;
 		int i = 1 ;
-		
-		while (i > 10) {
+
+		while (i <= 10) {
 			total += i;
 			i++;
 		}
