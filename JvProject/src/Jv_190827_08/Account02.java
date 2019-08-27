9ac4bdc9982f3feef6ad6ecfc851e766b75dda85
rap0d <changsh9155@naver.com>
2019-08-27 10:22:12 +0900

    update file

---
+++ JvProject/src/Jv_190827_08/Account02.java
@@ -0,0 +1,8 @@
+package Jv_190827_08;
+
+public class Account02 {
+	String bankname;
+	String name;
+	int no;
+	int balance;
+}
---
+++ JvProject/src/Jv_190827_08/AccountMain02.java
@@ -0,0 +1,8 @@
+package Jv_190827_08;
+
+public class AccountMain02 {
+public static void main(String[] args) {
+	Account02 a2 = new Account02();
+	a2.bankname = "KB";
+}
+}
