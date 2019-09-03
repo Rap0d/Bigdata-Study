f6e84e76d29ac5d8092a011f3473a23a08253810
rap0d <changsh9155@naver.com>
2019-09-03 18:05:36 +0900

    update file

--- JvProject/src/Jv_190903_13/Final01.java
+++ JvProject/src/Jv_190903_13/Final01.java
@@ -8,16 +8,28 @@ public class Final01 {
     public static void main(String[] args) {
         // Mclass.message = ""; 
         Cclass obj1 = new Cclass();
+        obj1.display();
+        obj1.some();
     }
 }
 
 class Mclass {
     static final String message = "Hello.";
     void display() {
-
+        System.out.println("HHi");
+    }
+    public final void some() {
+        System.out.println("final");
     }
 }
 
 class Cclass extends Mclass {
-
+    @Override
+    void display() {
+        System.out.println("hi");
+    }
+    // @Override
+    // void some() {
+    //     System.out.println("no");
+    // }
 }
